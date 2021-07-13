3c9f1d526fff305bec57fd4a8ca5d8275f38adf5
yktn3110 <yuki.3110f@gmail.com>
2021-07-14 06:33:00 +0900

    add OutputAddr.c and .exe

---
+++ Test_OutputAddr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global_variable;
+static int file_static_variable;
+
+void func1(void)
+{
+    int func1_varialbe;
+    static int local_static_variable;
+
+    printf("&func1_variable..%p\n", (void *)&func1_varialbe);
+    printf("&local_static_variable..%p\n", (void *)&local_static_variable);
+}
+
+void func2(void)
+{
+    int func2_variable;
+    printf("&func2_variable..%p\n", (void *)&func2_variable);
+}
+
+void main(void)
+{
+    int *p;
+
+    /* 関数へのポインタの表示 */
+    printf("func1..%p\n", (void *)func1);
+    printf("func2..%p\n", (void *)func2);
+
+    /* 文字列リテラルのアドレスの表示 */
+    printf("stirng literal..%p\n", (void *)"abc");
+
+    /* グローバル変数のアドレスの表示 */
+    printf("&glo0bal_variable..%p\n", (void *)&global_variable);
+
+    /* ファイル内static変数のアドレスの表示 */
+    printf("&file_static_variable..%p\n", (void *)&file_static_variable);
+
+    /* ローカル変数の表示 */
+    func1();
+    func2();
+
+    /* mallocにより確保した領域のアドレス */
+    p = malloc(sizeof(int));
+    printf("malloc address..%p\n", (void *)&p);
+
+    return 0;
+}
